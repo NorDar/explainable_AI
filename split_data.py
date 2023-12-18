058dc575647af9ede41b416da82fb82c7d62c8f4
Nordin Dari <dari@zhaw.ch>
2023-12-18 22:42:50 +0000

    first transform refactor finished.

--- split_data.py
+++ split_data.py
@@ -1,3 +1,4 @@
+from typing import List, Tuple
 import logging
 import h5py
 import pandas as pd
@@ -9,17 +10,20 @@ import seaborn as sns
 from sklearn.model_selection import StratifiedKFold
 
 
-def load_transform_dataset(path_img:str, 
-                 path_tab:str, 
-                 only_non_tia:bool, 
-                 output_dir:str,
-                 relevant_features:list,
-                 favorable_mrs:list,
-                 n_splits:int,
-                 split_version:str = None):
+def load_transform_dataset(
+        tabular_input_path:str,
+        image_input_path:str, 
+        tabular_output_path:str,
+        image_output_path:str,
+        only_non_tia:bool,
+        relevant_features:list,
+        favorable_mrs:list,
+        n_splits:int,
+        split_version:str,
+        ) -> None:
     
     # Reading the images
-    with h5py.File(path_img, "r") as h5:
+    with h5py.File(image_input_path, "r") as h5:
         X_in = h5["X"][:]
         Y_img = h5["Y_img"][:]
         Y_pat = h5["Y_pat"][:]
@@ -30,7 +34,7 @@ def load_transform_dataset(path_img:str,
     logging.info(f"image min: {X_in.min()}, max: {X_in.max()}, mean: {X_in.mean()}, std: {X_in.std()}")
 
     # Read tabular data
-    dat = pd.read_csv(path_tab, sep=",")
+    dat = pd.read_csv(tabular_input_path, sep=",")
     logging.info("tabular shape in: ", dat.shape)
 
     # Get original data    
@@ -41,7 +45,8 @@ def load_transform_dataset(path_img:str,
     n = len(n)
 
     # Match image and tabular data
-    # TODO: Can be optimized: preinitialization neccessary?
+    # For patients that are in both sets we fill the neccessary datastructures
+    # TODO: Can be optimized? Preinitialization neccessary?
     X = np.zeros((n, X_in.shape[1], X_in.shape[2], X_in.shape[3], X_in.shape[4]))
     X_tab = np.zeros((n, len(relevant_features)))
     Y_mrs = np.zeros((n))
@@ -63,7 +68,11 @@ def load_transform_dataset(path_img:str,
         else:
             non_matched_patients.append(p)
     p_id = p_id.astype("int")
-            
+    
+    
+    X = X.squeeze()
+    X = np.float32(X)
+
     logging.info("X img out shape: ", X.shape)
     logging.info("X tab out shape: ", X_tab.shape)
     logging.info("Y mrs out shape: ", Y_mrs.shape)
@@ -109,6 +118,8 @@ def load_transform_dataset(path_img:str,
         }
     )
 
+    # TODO: Move this to config.. Only neccessary for tracking by papers.
+    # TODO: Move this out of method -> this info/logic should not be kept here.
     # Create StratifiedKFold object.
     # 10 Fold V0 random_state 100
     # 10 Fold V1 random_state 999
@@ -122,19 +133,21 @@ def load_transform_dataset(path_img:str,
     }
 
     if not split_version:
-        logging.warning("Split version is not set so default value for random seed of split 'V3' is used")
+        logging.error("Split version is not set. We quit this now.")
+        raise ValueError("Split version is not set.")
     elif split_version not in split_dict:
-        logging.warning(f"Split version '{split_version}' is not in {split_dict}. Using default value of 'V3'")
+        logging.error(f"Split version '{split_version}' is not in {split_dict}. We quit this now.")
+        raise ValueError(f"Split version '{split_version}' is not in {split_dict}.")
     else:
         logging.info(f"Using random seed for split '{split_version}: {split_version.get(split_version)}'")
     
     
     skf = StratifiedKFold(n_splits=n_splits, 
                           shuffle=True, 
-                          random_state=split_dict.get(split_version, 200))
+                          random_state=split_dict[split_version])
     folds = []
     
-    # TODO: Here we only spit out info for binary... Ordinal output (Y_mrs) not handled.
+    # TODO: Here we only split out info for binary... Ordinal output (Y_mrs) not handled?
     logging.info("Splitting by binned target variable Y_new:") 
     for train_index, test_index in skf.split(p_id, Y_new): # 10 Fold sigmoid stratified with Outcome Good/Bad (V0, V2, V3)
     # for train_index, test_index in skf.split(p_id, Y_mrs): # 10 Fold sigmoid stratified with Outcome MRS (V1)
@@ -146,62 +159,74 @@ def load_transform_dataset(path_img:str,
         logging.info(f"{len(fold)} samples in fold {i}")
         id_tab["fold" + str(i)] = "train" 
         
-        # increment for val (+5 so that no fold has only 40 in train & test)
+        # increment for val (+5 so that no fold has only 40 in train & test) # TODO: what does this mean???
         j = i+5
         if j >= len(folds):
-            j = j-10
+            j = j-n_splits
 
         id_tab.loc[id_tab["p_id"].isin(fold), "fold"+str(i)] = "test"
         id_tab.loc[id_tab["p_id"].isin(folds[j]), "fold"+str(i)] = "val"
 
 
+    # Finally we save
+    id_tab.to_csv(tabular_output_path, index=False)
+    np.save(image_output_path, X)
 
-    # Save Data
-        
-    # id_tab.to_csv(OUTPUT_DIR + "10Fold_ids_V0.csv",  index=False)
-    # id_tab.to_csv(OUTPUT_DIR + "10Fold_ids_V1.csv",  index=False)
-    # id_tab.to_csv(OUTPUT_DIR + "10Fold_ids_V2.csv",  index=False)
-    id_tab.to_csv(OUTPUT_DIR + "10Fold_ids_V3.csv",  index=False)
-
-
-    # Is the same for all versions but is generated nonetheless, could als be generated only once
-    X = X.squeeze()
-    X = np.float32(X)
-
-    np.save(OUTPUT_DIR + "prepocessed_dicom_3d.npy", X)
+    # TODO: Return paths saved? decide later
+    return None
 
 
+def validate_transformed_data(
+        patient_id:int, 
+        image_input_path:str,
+        tabular_output_path:str, 
+        image_output_path:str,
+        ) -> Tuple[bool, int, int]:
+    """
+    Validate that the patient with id patient_id actually has the correct data linked 
+    from before and after the transformation
+    """
+    
+    # load the transformed data
+    id_tab = pd.read_csv(tabular_output_path, sep=",")
+    X = np.load(image_output_path)
 
-    # Analyze Data
-    # id_tab = pd.read_csv(OUTPUT_DIR + "10Fold_ids_V0.csv", sep=",")
-    # id_tab = pd.read_csv(OUTPUT_DIR + "10Fold_ids_V1.csv", sep=",")
-    # id_tab = pd.read_csv(OUTPUT_DIR + "10Fold_ids_V2.csv", sep=",")
-    id_tab = pd.read_csv(OUTPUT_DIR + "10Fold_ids_V3.csv", sep=",")
-    X = np.load(OUTPUT_DIR + "prepocessed_dicom_3d.npy")
+    # load the raw data
+    with h5py.File(image_input_path, "r") as h5:
+        X_in = h5["X"][:]
+        _ = h5["Y_img"][:]
+        _ = h5["Y_pat"][:]
+        pat = h5["pat"][:]
+    
 
-
-    id_tab["unfavorable"].value_counts()
-
-    for i in range(10):
-        fig, (ax1, ax2) = plt.subplots(1,2)
-        sns.countplot(x = id_tab[id_tab["fold"+str(i)]=="test"].mrs, ax = ax1)
-        sns.countplot(x = id_tab[id_tab["fold"+str(i)]=="test"].unfavorable, ax = ax2)
-
-    # Check Images
-        
-
-    patient = 460
-    index1 = id_tab[id_tab.p_id == patient].p_idx.values[0] -1
-    # index1 = id_tab[id_tab.p_id == patient].index
-    print(index1)
-    index2 = np.argwhere(pat == patient).squeeze()
-    print(index2)
-
+    index1 = id_tab[id_tab.p_id == patient_id].p_idx.values[0] -1
+    logging.info(f"DataFrame index of patient {patient_id} in the transformed data: {index1}.")
+    index2 = np.argwhere(pat == patient_id).squeeze()
+    logging.info(f"DataFrame index of patient {patient_id} in the raw data: {index2}.")
 
 
     im1 = X[index1].astype("float64")
     im2 = X_in.squeeze()[index2].astype("float64")
-    np.allclose(im1, im2)
+    data_is_equals = np.allclose(im1, im2)
+    assert data_is_equals, f"The patients data, for id {patient_id}, before and after transformation is not equals."
+    logging.info(f"The data for patient {patient_id} is equals")
+
+    return data_is_equals, index1, index2
+
+
+def plot_info_split_data(tabular_output_path:str, n_splits:int = 10) -> None:
+    id_tab = pd.read_csv(tabular_output_path, sep=",")
+
+
+    id_tab["unfavorable"].value_counts()
+
+    for i in range(n_splits):
+        fig, (ax1, ax2) = plt.subplots(1,2)
+        sns.countplot(x = id_tab[id_tab["fold"+str(i)]=="test"].mrs, ax = ax1)
+        sns.countplot(x = id_tab[id_tab["fold"+str(i)]=="test"].unfavorable, ax = ax2)
+
+    return None
+
 
 def plot_count_mrs_binned_mrs(Y_mrs, Y_new):
     fig, (ax1, ax2) = plt.subplots(1,2, figsize=(15, 8))
@@ -213,29 +238,45 @@ def plot_count_mrs_binned_mrs(Y_mrs, Y_new):
     print(f"Binary count: Good outcome: {sum(Y_new == 0)}, Bad outcome{sum(Y_new == 1)}")
 
 def main():
-    IMG_DIR = "/tf/notebooks/hezo/stroke_perfusion/data/"
-    path_img = IMG_DIR + 'dicom_2d_192x192x3_clean_interpolated_18_02_2021_preprocessed2.h5'
-    path_tab = IMG_DIR + 'baseline_data_zurich_prepared.csv'
-
-    OUTPUT_DIR = "/tf/notebooks/schnemau/xAI_stroke_3d/data/"
+    SPLIT_VERSION = "V3"
+    N_SPLITS = 10
 
     # should only non TIA (transient ischemic attack) patients be included?
     only_non_tia = True
+
+    IMG_DIR = "/tf/notebooks/hezo/stroke_perfusion/data/"
+    RAW_TABULAR = IMG_DIR + 'baseline_data_zurich_prepared.csv'
+    RAW_IMAGES = IMG_DIR + 'dicom_2d_192x192x3_clean_interpolated_18_02_2021_preprocessed2.h5'
+
+    OUTPUT_DIR = "/tf/notebooks/schnemau/xAI_stroke_3d/data/"
+    TRANSFORMED_TABULAR = f"{OUTPUT_DIR}{N_SPLITS}Fold_ids_{SPLIT_VERSION}.csv"
+    TRANSFORMED_IMAGES = f"{OUTPUT_DIR}prepocessed_dicom_3d.npy"
 
     relevant_features = ["age", "sexm", "nihss_baseline", "mrs_before",
                         "stroke_beforey", "tia_beforey", "ich_beforey", 
                         "rf_hypertoniay", "rf_diabetesy", "rf_hypercholesterolemiay", 
                         "rf_smokery", "rf_atrial_fibrillationy", "rf_chdy"]
 
-    d = load_transform_dataset(path_img=path_img, 
-                     path_tab=path_tab, 
-                     only_non_tia=only_non_tia, 
-                     output_dir=OUTPUT_DIR,
-                     relevant_features=relevant_features,
-                     favorable_mrs=[0,1,2],
-                     n_splits=10,
-                     split_version='V3')
+    load_transform_dataset(
+        tabular_input_path=RAW_TABULAR, 
+        image_input_path=RAW_IMAGES, 
+        tabular_output_path=TRANSFORMED_TABULAR,
+        image_output_path=TRANSFORMED_IMAGES,
+        only_non_tia=only_non_tia, 
+        output_dir=OUTPUT_DIR,
+        relevant_features=relevant_features,
+        favorable_mrs=[0,1,2],
+        n_splits=N_SPLITS,
+        split_version=SPLIT_VERSION,
+        )
     
+    data_is_valid, id_before_transform, id_after_transform = validate_transformed_data(
+        patient_id=460, 
+        image_input_path=RAW_IMAGES, 
+        tabular_output_path=TRANSFORMED_TABULAR,
+        image_output_path=TRANSFORMED_IMAGES
+        )
+
 
     
 
